0737ed60849ccd1bf86c100fee4cc7040d3e9e65
Supreet <supreetkumar32@gmail.com>
2025-09-16 22:44:16 +0530

    setup complete

--- src/main/java/com/three/jpaTutorial/jpaTuts/entities/Product.java
+++ src/main/java/com/three/jpaTutorial/jpaTuts/entities/Product.java
@@ -1,13 +1,12 @@
 package com.three.jpaTutorial.jpaTuts.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -18,21 +17,35 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Builder
 @Entity
+@Table(
+        name="product_table",
+        uniqueConstraints = {
+                @UniqueConstraint(name="sku_unique",columnNames ={"sku"}),
+                @UniqueConstraint(name="title_price_unique",columnNames ={"title_x","price"})
+        },
+        indexes={
+                @Index(name="sku_index",columnList="sku")
+        }
+)
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable=false,length =20)
     private String sku;
 
+    @Column(name="title_x")
     private String title;
 
     private BigDecimal price;
 
     private Integer quantity;
 
+    @CreationTimestamp
     private LocalDateTime createdAt;
 
+    @UpdateTimestamp
     private LocalDateTime updatedAt;
 
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into sys.product_table(price,quantity,sku,title_x)
+values
+(12.4,4,'parle734','Parle Biscuit'),
+(14.4,1,'pepsi123','Pepsi'),
+(16.8,8,'coca123','coca'),
+(16.4,3,'pa734','it');
