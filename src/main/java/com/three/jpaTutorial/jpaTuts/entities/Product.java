4f1af612c03582fc5fa47b2d1e2cc3de57e0e748
Supreet <supreetkumar32@gmail.com>
2025-09-16 07:10:01 +0530

    initial commit for JPA

---
+++ src/main/java/com/three/jpaTutorial/jpaTuts/entities/Product.java
@@ -0,0 +1,6 @@
+package com.three.jpaTutorial.jpaTuts.entities;
+
+public class Product {
+
+
+}
---
+++ src/test/java/com/three/jpaTutorial/jpaTuts/JpaTutsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.three.jpaTutorial.jpaTuts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaTutsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
