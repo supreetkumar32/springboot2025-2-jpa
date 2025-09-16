2853f4cebe9213035962951eb3b097f8d2e6135e
Supreet <supreetkumar32@gmail.com>
2025-09-16 07:38:50 +0530

    basic configuration

--- src/main/java/com/three/jpaTutorial/jpaTuts/entities/Product.java
+++ src/main/java/com/three/jpaTutorial/jpaTuts/entities/Product.java
@@ -1,6 +1,38 @@
 package com.three.jpaTutorial.jpaTuts.entities;
 
-public class Product {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String sku;
+
+    private String title;
+
+    private BigDecimal price;
+
+    private Integer quantity;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
 
 }
