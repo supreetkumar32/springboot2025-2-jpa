8b1333152f9ee2d913e7827d91dbe4cde449b178
Supreet <supreetkumar32@gmail.com>
2025-09-17 21:40:31 +0530

    method names for querying and custom query using jpql

--- src/main/java/com/three/jpaTutorial/jpaTuts/repositories/ProductRepository.java
+++ src/main/java/com/three/jpaTutorial/jpaTuts/repositories/ProductRepository.java
@@ -2,8 +2,33 @@ package com.three.jpaTutorial.jpaTuts.repositories;
 
 import com.three.jpaTutorial.jpaTuts.entities.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity,Long> {
+    List<ProductEntity> findByTitle(String title);
+
+    List<ProductEntity> findByCreatedAtAfter(LocalDateTime after);
+
+    List<ProductEntity> findByQuantityAndPrice(int quantity, BigDecimal price);
+
+    List<ProductEntity> findByQuantityGreaterThanAndPriceLessThan(int quantity, BigDecimal price);
+
+    List<ProductEntity> findByTitleLike(String title);
+
+    List<ProductEntity> findByTitleContaining(String title);
+
+    List<ProductEntity> findByTitleContainingIgnoreCase(String title);
+
+   //Optional<ProductEntity> findByTitleAndPrice(String title, BigDecimal price); //as title an dprice combination we hav egiven in the constratint taht coombination of both is unique..so it will return only one element or null
+
+    //@Query("select e from ProductEntity e where e.title=?1 and e.price=?2")//here we have to give java class and variable name as it is JPQL .
+    @Query("select e.title from ProductEntity e where e.title=?1 and e.price=?2")
+    Optional<ProductEntity> findByTitleAndPrice(String title, BigDecimal price);
 }
--- src/test/java/com/three/jpaTutorial/jpaTuts/JpaTutsApplicationTests.java
+++ src/test/java/com/three/jpaTutorial/jpaTuts/JpaTutsApplicationTests.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 class JpaTutsApplicationTests {
@@ -22,7 +24,7 @@ class JpaTutsApplicationTests {
 	@Test
 	void testRepository(){
 		ProductEntity productEntity=ProductEntity.builder()
-				.sku("nestle234")
+				.sku("nestle2345")
 				.title("Nestle chocolate")
 				.price(BigDecimal.valueOf(123.45))
 				.quantity(12)
@@ -34,8 +36,24 @@ class JpaTutsApplicationTests {
 
 	@Test
 	void getRepository(){
-		List<ProductEntity> entities=productRepository.findAll();
+//		List<ProductEntity> entities=productRepository.findAll();
+//		List<ProductEntity> entities=productRepository.findByTitle("Pepsi");
+//		List<ProductEntity> entities=productRepository.findByCreatedAtAfter(
+//				LocalDateTime.of(2024,1,1,0,0,0));
+		//List<ProductEntity> entities=productRepository.findByQuantityAndPrice(8,BigDecimal.valueOf(16.8));
+		//List<ProductEntity> entities=productRepository.findByQuantityGreaterThanAndPriceLessThan(8,BigDecimal.valueOf(16.8));
+		//List<ProductEntity> entities=productRepository.findByTitleLike("%choco%");
+//		List<ProductEntity> entities=productRepository.findByTitleContaining("choco");
+		List<ProductEntity> entities=productRepository.findByTitleContainingIgnoreCase("chOCo");
 		System.out.println(entities);
 	}
+
+	@Test
+	void getSingleFromRepository(){
+		Optional<ProductEntity> productEntity=productRepository
+				.findByTitleAndPrice("Pepsi",BigDecimal.valueOf(14.4));
+		productEntity.ifPresent(System.out::println);
+	}
+
 
 }
